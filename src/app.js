7e8cd2990de20f768e3d85fa219bfecbd6a537e6
ganesh <ganesh@gmail.com>
2024-09-21 18:48:21 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app= express();
+
+app.use("/test",(req,res)=>{
+    res.send("dshgfjysdgcfhdbvg");
+});
+
+app.use((req,res)=>{
+    res.send("as soon as possible")
+
+});
+
+app.use((req,res)=>{
+    res.send("sdufgudscgvhmdbcvjhyydgd");
+});
+
+app.listen(3000,()=>{
+    console.log("server is sucessfully listenting on port777");
+
+});
