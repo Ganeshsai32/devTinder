5d9bec603a5ca432152ea2eb0c13c05f6c5b6dcc
ganesh <ganesh@gmail.com>
2024-09-28 23:59:40 +0530

    api calls update delete

--- src/app.js
+++ src/app.js
@@ -8,13 +8,63 @@ const cors= require('cors');
 app.use(cors());
 
 
-
 app.post("/signup", async (req,res)=>{
-    const user = new User(req.body);
 
-    await user.save();
-    res.send("use added sucessfully");
+    const user= new User (req.body);
+
+    try {
+        await user.save();
+        res.send("user data sucessfully");
+    }
+    catch(err){
+        res.status(400).send("Error saving the user :" + err.message);
+    }
+
 });
+ 
+
+app.get("/user",async( req,res) => {
+
+const userEmail = req.body.emailId;
+
+try{
+    const user =await User.find({emailId:userEmail});
+    res.send(user);
+}
+
+catch(err) {
+    res.status(400).send("something went wrong");
+
+}
+});
+
+
+
+app.get("/feed", async (req,res)=>{
+    
+    try{
+
+        const users=await User.find({});
+        res.send(users);
+
+
+    } catch(err){
+        res.status(400).send("something wromg");
+
+    }
+});
+
+app.delete("/user",async (req,res)=>{
+    const userId = req.body.userId;
+   try{
+      const user=await User.findByIdAndDelete(userId);
+      res.send("data added suceesfullly");
+   }catch(err){
+      res.status(400).send("something went wrong");
+
+   }
+});
+
 
 
 connectDB()
